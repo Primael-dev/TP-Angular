9cf917670e803b74ca2360f1b704417a565bf742
Prunelle <prunelledalmeida500@gmail.com>
2025-07-24 16:53:59 +0200

    Header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
