220d3ad8f480c6543217686205d9e08b5a51b4f4
precieusedjossou <nellycedjossouprecieuse@gmail.com>
2025-07-25 02:16:56 +0100

    envoie contact

--- src/app/contact-page/contact-page.ts
+++ src/app/contact-page/contact-page.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-contact-page',
-  imports: [],
-  templateUrl: './contact-page.html',
-  styleUrl: './contact-page.css'
+  selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  template: './contact.html',
+  styles: './contact.css' ,
 })
-export class ContactPage {
+export class ContactComponent {
+  form = {
+    name: '',
+    email: '',
+    phone: '',
+    message: ''
+  };
 
+  successMessage = '';
+
+  onSubmit() {
+    console.log('Formulaire soumis', this.form);
+
+    this.successMessage = "✅ Votre message a été envoyé avec succès !";
+
+    // Réinitialiser le formulaire
+    this.form = {
+      name: '',
+      email: '',
+      phone: '',
+      message: ''
+    };
+  }
 }
